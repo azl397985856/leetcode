06479cc09b060f6edf52ec9a18a1af0434083f32
luzhipeng <luzhipeng@duiba.com.cn>
2019-03-25 13:53:19 +0800

    😁  stu

---
+++ 279.perfect-squares.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=279 lang=javascript
+ *
+ * [279] Perfect Squares
+ *
+ * https://leetcode.com/problems/perfect-squares/description/
+ *
+ * algorithms
+ * Medium (40.98%)
+ * Total Accepted:    168.2K
+ * Total Submissions: 408.5K
+ * Testcase Example:  '12'
+ *
+ * Given a positive integer n, find the least number of perfect square numbers
+ * (for example, 1, 4, 9, 16, ...) which sum to n.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 12
+ * Output: 3 
+ * Explanation: 12 = 4 + 4 + 4.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 13
+ * Output: 2
+ * Explanation: 13 = 4 + 9.
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+    
+};
+
