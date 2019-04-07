54d38e405b57e1ad9c6b18b91bdfe3748deaa6e9
luzhipeng <luzhipeng@duiba.com.cn>
2019-04-07 17:01:34 +0800

    更新游程编码和huffman编码

---
+++ 416.partition-equal-subset-sum.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=416 lang=javascript
+ *
+ * [416] Partition Equal Subset Sum
+ *
+ * https://leetcode.com/problems/partition-equal-subset-sum/description/
+ *
+ * algorithms
+ * Medium (39.97%)
+ * Total Accepted:    79.7K
+ * Total Submissions: 198.5K
+ * Testcase Example:  '[1,5,11,5]'
+ *
+ * Given a non-empty array containing only positive integers, find if the array
+ * can be partitioned into two subsets such that the sum of elements in both
+ * subsets is equal.
+ * 
+ * Note:
+ * 
+ * 
+ * Each of the array element will not exceed 100.
+ * The array size will not exceed 200.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1, 5, 11, 5]
+ * 
+ * Output: true
+ * 
+ * Explanation: The array can be partitioned as [1, 5, 5] and [11].
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1, 2, 3, 5]
+ * 
+ * Output: false
+ * 
+ * Explanation: The array cannot be partitioned into equal sum subsets.
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+    
+};
+
