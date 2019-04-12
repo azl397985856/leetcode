db3aee9346433f40b59d0d35a8ed73fcd69c362c
luzhipeng <luzhipeng@duiba.com.cn>
2019-04-12 14:23:31 +0800

    更新readme

---
+++ todo/240.search-a-2-d-matrix-ii.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=240 lang=javascript
+ *
+ * [240] Search a 2D Matrix II
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (40.30%)
+ * Total Accepted:    170K
+ * Total Submissions: 419.1K
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * 
+ * 
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * 
+ * 
+ * Example:
+ * 
+ * Consider the following matrix:
+ * 
+ * 
+ * [
+ * ⁠ [1,   4,  7, 11, 15],
+ * ⁠ [2,   5,  8, 12, 19],
+ * ⁠ [3,   6,  9, 16, 22],
+ * ⁠ [10, 13, 14, 17, 24],
+ * ⁠ [18, 21, 23, 26, 30]
+ * ]
+ * 
+ * 
+ * Given target = 5, return true.
+ * 
+ * Given target = 20, return false.
+ * 
+ */
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    
+};
+
