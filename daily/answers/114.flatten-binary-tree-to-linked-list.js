f986c26422e8e0eea49478ff8c10708686c6d5ee
luzhipeng <luzhipeng@duiba.com.cn>
2019-06-17 09:58:26 +0800

    feat: 每日一题更新参考答案

---
+++ daily/answers/114.flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=114 lang=javascript
+ *
+ * [114] Flatten Binary Tree to Linked List
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+function preorderTraversal(root) {
+  if (!root) return [];
+
+  return [root]
+    .concat(preorderTraversal(root.left))
+    .concat(preorderTraversal(root.right));
+}
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    if (root === null) return root;
+    const res = preorderTraversal(root);
+    
+    let curPos = 0;
+    let curNode = res[0];
+
+    while(curNode = res[curPos]) {
+        curNode.left = null;
+        curNode.right = res[++curPos];
+    }
+};
