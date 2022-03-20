d3a3e301cd371a696996e8ad0cc9da94df1cb124
robot <xifa@tsign.cn>
2022-03-20 16:34:30 +0800

    ci: mdl

---
+++ .github/workflows/check-format.js
@@ -0,0 +1,30 @@
+// const util = require("util");
+// const glob = util.promisify(require('glob'));
+// const fs = require("fs").promises;
+// const path = require('path');
+
+
+// async function main() {
+//     var errors = [];
+//     var directories = await glob(__dirname + '../../**/*.md');
+
+//     for (var filePath of directories) {
+//         var data = await fs.readFile(filePath, 'utf8');
+//         var filename = path.parse(filePath).base.replace(".md", "");
+
+//         // do check
+//     }
+
+//     if (errors.length > 0) {
+//         for (var error of errors) {
+//             console.error(error + "\n");
+//         }
+
+//         var message = `Found ${errors.length} errors! Please fix!`;
+//         throw new Error(message);
+//     }
+// }
+
+// main();
+
+console.log('TO BE IMPLEMENTED')
---
+++ .mdl_style.rb
@@ -0,0 +1,7 @@
+all
+# 我想让图片居中，单纯的 md 似乎做不到，所以使用 raw html 来实现
+exclude_rule 'MD033'
+# 仅仅是强调，不需要用 header
+exclude_rule 'MD036'
+# 这个 rule 有 bug?
+exclude_rule 'MD029'
